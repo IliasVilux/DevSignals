26de34285d139484ad3af8f5a1b3199093a371c1
IliasVilux <iliasotsman@gmail.com>
2026-02-27 01:46:01 +0100

    chore: :wrench: Configure CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import marketRoutes from './routes/market.routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({ origin: ['http://localhost:5173', 'https://your-app.vercel.app'] }));
 app.use(express.json());
 
 app.use("/api/market", marketRoutes);
